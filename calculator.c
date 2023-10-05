d13a05c510c759c24aff52f8656f3350b98a3819
Keyrif <mihaikenj@gmail.com>
2023-10-05 15:51:23 +0300

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -6,10 +6,11 @@ int main() {
   char operator;
   int input1;
   int input2;
-
+  printf("> ");
   int inputs = scanf("%d %c %d", &input1, &operator, &input2);
 
   if (inputs == 3) {
+    printf("> ");
     if (operator == '+') printf("Result: %d", input1 + input2);
     if (operator == '-') printf("Result: %d", input1 - input2);
     if (operator == '*') printf("Result: %d", input1 * input2);
