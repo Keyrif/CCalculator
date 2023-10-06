08a3c1611a4f48404fbb8a276bc4b688ed288a7f
Keyrif <mihaikenj@gmail.com>
2023-10-06 13:15:34 +0300

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -9,13 +9,9 @@ int main() {
   printf("> ");
   int inputs = scanf("%d %c %d", &input1, &operator, &input2);
 
-  if (inputs != 3){
-    printf("ERROR: Invalid number(s)!");
-    return 0;
-  }
   
 
-  if (operator == '+' || operator == '-' || operator == '*' || operator == '/') {
+  if (inputs == 3 && operator == '+' || operator == '-' || operator == '*' || operator == '/') {
     printf("> ");
     if (operator == '+') printf("Result: %d", input1 + input2); else if (operator == '-') {
       printf("Result: %d", input1 - input2);
@@ -26,6 +22,7 @@ int main() {
     }
   } else {
     puts("ERROR: There are not enough numbers or operator is invalid!");
+    return 0;
   }
   
 }
